cc6ba7305eac1cd9d82da774c9d668507346738e
Juan Camilo Solano Estrada <juacase5826@outlook.com>
2021-09-14 01:28:43 -0700

    Added method to add fake data to db.

--- src/models/board/board.service.ts
+++ src/models/board/board.service.ts
@@ -1,4 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { BoardEntity } from 'src/models/board/board.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class BoardService {}
+export class BoardService implements OnModuleInit {
+  constructor(
+    @InjectRepository(BoardEntity)
+    private boardEntityRepository: Repository<BoardEntity>,
+  ) {}
+
+  async onModuleInit() {
+    const [dbBoard, dbBoard2] = await this.boardEntityRepository.find();
+
+    if (!dbBoard) {
+      const boardEntity = this.boardEntityRepository.create({
+        name: 'board',
+      });
+      await this.boardEntityRepository.save(boardEntity);
+    }
+
+    if (!dbBoard2) {
+      const boardEntity = this.boardEntityRepository.create({
+        name: 'board2',
+      });
+      await this.boardEntityRepository.save(boardEntity);
+    }
+  }
+}
