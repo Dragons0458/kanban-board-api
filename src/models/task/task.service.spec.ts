25986eb370b2cc276390f977fc1150f1f9d224d6
Juan Camilo Solano Estrada <juacase5826@outlook.com>
2021-09-16 16:14:47 -0700

    Added task service tests.

--- src/models/task/task.service.spec.ts
+++ src/models/task/task.service.spec.ts
@@ -1,18 +1,39 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { TaskEntity } from 'src/models/task/task.entity';
 import { TaskService } from './task.service';
 
 describe('TaskService', () => {
+  const repositoryMock = {
+    find: jest.fn(),
+    create: jest.fn(),
+    save: jest.fn(),
+    update: jest.fn(),
+  };
+
   let service: TaskService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TaskService],
+      providers: [
+        TaskService,
+        {
+          provide: getRepositoryToken(TaskEntity),
+          useValue: repositoryMock,
+        },
+      ],
     }).compile();
 
     service = module.get<TaskService>(TaskService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('checks onModuleInit method', async () => {
+    expect.assertions(2);
+    repositoryMock.find.mockClear();
+    repositoryMock.find.mockResolvedValue([]);
+    await service.onModuleInit();
+
+    expect(repositoryMock.create).toBeCalledTimes(4);
+    expect(repositoryMock.save).toBeCalledTimes(4);
   });
 });
